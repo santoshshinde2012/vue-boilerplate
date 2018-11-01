e5de314f7ebde29ee734132a3df7058ac41f27d8
santosh shinde <santosh.shinde@wwindia.com>
2018-11-01 10:02:40 +0530

    Updates

---
+++ precache-manifest.6293273d5fc98776d246b6d970419029.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "79947846c89f5c8de818",
+    "url": "/js/chunk-vendors.462efa5d.js"
+  },
+  {
+    "revision": "763db989c91433aeed9581fa27b78e9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83880f352fe065a2a7c",
+    "url": "/js/chunk-01c195d5.447a5d73.js"
+  },
+  {
+    "revision": "7ecc919a7e0fc86f2820",
+    "url": "/js/chunk-01d9b17c.37172984.js"
+  },
+  {
+    "revision": "50498597b9b0d10c10f2",
+    "url": "/js/chunk-33fb2e38.91431a2a.js"
+  },
+  {
+    "revision": "2d28de44198de41cfefc",
+    "url": "/js/chunk-33fc0274.4afa3ff0.js"
+  },
+  {
+    "revision": "d2cebf7dd0b27cea2e55",
+    "url": "/js/app.801ef0b2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "79947846c89f5c8de818",
+    "url": "/css/chunk-vendors.fdcf0fda.css"
+  },
+  {
+    "revision": "d2cebf7dd0b27cea2e55",
+    "url": "/css/app.f9398c35.css"
+  }
+];
